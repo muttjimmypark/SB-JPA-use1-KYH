7012a4b252caaeb4a9c5c012fb6cc63c821650d0
muttjimmypark <muttpark57@gmail.com>
2023-01-06 15:14:15 +0900

    회원목록조회 기능 구현 : 엔티티 필드를 외부로 반환해선 안된다

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -33,7 +35,7 @@ public class MemberController {
          *
          * createMemberForm의 name필드에서 처리해준덕분에 @NotNull의 에러메시지를 나타나게
          * POST로 MemberForm 정보들도 같이 넘어가니까, 다른필드에서 작성한게 유지도 됨
-         *          */
+         */
 
         if (result.hasErrors()) {
             return "members/createMemberForm";
@@ -50,8 +52,24 @@ public class MemberController {
 
         /**
          * 화면~컨트롤러 간 폼데이터를 별도로 만들어서
-         * 도메인데이터 코드가 지저분해지는것을 막고
+         * 화면에서만 필요한 기능때문에 도메인데이터 코드가 지저분해지는것을 막고
          * 서로 다를 수 있는 제약조건을 별도로 관리할수 있다
+         *
+         * 도메인이 오직 핵심비즈니스로직에만 의존하도록 순수하게 작성하는것이 중요
+         * 유지보수성을 위해 : 폼객체, dto 등을 별도 사용
+         */
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+        /**
+         * 템플릿엔진에는 선택적으로 엔티티필드를 전달할수 있지만
+         * api를 만들때는 엔티티를 외부로 반환해선 안된다
+         * 엔티티에 필드나 로직을 추가했을때 api의 스펙이 변해버리기 때문
          */
     }
 }
